29992e957e9ca8a0f5d30822aa8cbd5f8b974c77
Yohei Ema <24579635+meihei3@users.noreply.github.com>
2022-07-23 17:32:36 +0900

    Revert "update init.sql"

--- sql/init.sql
+++ sql/init.sql
@@ -3,18 +3,14 @@ DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
 
--- CREATE TABLE IF NOT EXISTS `visit_history_first_created_at` (
---   `id` BIGINT NOT NULL AUTO_INCREMENT,
---   `player_id` VARCHAR(255) NOT NULL,
---   `tenant_id` BIGINT UNSIGNED NOT NULL,
---   `competition_id` VARCHAR(255) NOT NULL,
---   `min_created_at` BIGINT NOT NULL,
---   PRIMARY KEY (`id`),
---   INDEX `for_min_created_at_idx` (`tenant_id`, `competition_id`, `player_id`)
--- ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
--- DELETE FROM visit_history_first_created_at WHERE id > 200474;
+CREATE TABLE IF NOT EXISTS `visit_history_first_created_at` (
+  `player_id` VARCHAR(255) NOT NULL,
+  `tenant_id` BIGINT UNSIGNED NOT NULL,
+  `competition_id` VARCHAR(255) NOT NULL,
+  `min_created_at` BIGINT NOT NULL,
+  PRIMARY KEY  (`tenant_id`, `competition_id`, `player_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+DELETE FROM visit_history_first_created_at;
 
--- INSERT INTO visit_history_first_created_at (player_id, tenant_id, competition_id, min_created_at)
--- SELECT player_id, tenant_id, competition_id, MIN(created_at) AS min_created_at FROM visit_history GROUP BY player_id, tenant_id, competition_id;
-
-DELETE FROM visit_history_first_created_at WHERE id > 200474;
+INSERT INTO visit_history_first_created_at (player_id, tenant_id, competition_id, min_created_at)
+SELECT player_id, tenant_id, competition_id, MIN(created_at) AS min_created_at FROM visit_history GROUP BY player_id, tenant_id, competition_id;
